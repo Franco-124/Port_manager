4481b279780b419c1236be3f7a873af6016b1eed
johan <johanstevenfrancoalvarez54@gmail.com>
2025-02-21 21:15:52 -0500

    Agregar archivos de proyecto.

---
+++ Clases/dbconexion.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace SqlConnectiondb
+{
+    class DatabaseHelper
+    {
+        private static string connectionString = "Server=localhost;Database=Port_manager;Integrated Security=True;";
+
+        //Aqui creamos la conexion a la db en sql server
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+
+        //Metodo para verificar el log in , usando getConection
+        public static bool VerificarLogin(string nombre, string email, string contraseña)
+        {
+            using (SqlConnection connection = GetConnection()) 
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("verificar_login", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@contraseña", contraseña);
+
+                        int resultado = Convert.ToInt32(cmd.ExecuteScalar());
+
+                        return resultado == 1;
+                    }
+                }
+                catch (SqlException sqlEx)
+                {
+                    Console.WriteLine("❌ Error de SQL: " + sqlEx.Message);
+                    return false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("❌ Error: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        // Método para agregar el usuario usando GetConnection()
+        public static bool agregar_usuario_ (string nombre, string email, string password)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection()) // 👈 Reutilizando GetConnection()
+                {
+                    using (SqlCommand cmd = new SqlCommand("agregar_usuario", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Parámetros de entrada
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@email", email);
+                        cmd.Parameters.AddWithValue("@contraseña", password);
+
+                        // Parámetro de salida
+                        SqlParameter outputParam = new SqlParameter("@resultado", SqlDbType.Int);
+                        outputParam.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(outputParam);
+
+                    
+                        cmd.ExecuteNonQuery(); // Ejecuta el procedimiento
+
+                        int resultado = Convert.ToInt32(outputParam.Value); // Obtiene el valor del parámetro de salida
+
+                        return resultado == 1;
+
+                    }
+                }
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine("❌ Error de SQL: " + sqlEx.Message);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("❌ Error: " + ex.Message);
+                return false;
+            }
+
+        }
+        public static bool verificar_nombre_usuario(string nombre)
+        {
+            try
+            {
+                using (SqlConnection connection = GetConnection()) 
+                {
+                    using (SqlCommand cmd = new SqlCommand("verificar_nombre_usuario", connection))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+
+                        // Parámetros de entrada
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        // Parámetro de salida
+                        SqlParameter outputParam = new SqlParameter("@existe", SqlDbType.Int);
+                        outputParam.Direction = ParameterDirection.Output;
+                        cmd.Parameters.Add(outputParam);
+
+
+                        cmd.ExecuteNonQuery(); // Ejecuta el procedimiento
+
+                        int resultado = Convert.ToInt32(outputParam.Value);
+
+                        return resultado == 1;
+
+                    }
+                }
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine("❌ Error de SQL: " + sqlEx.Message);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("❌ Error: " + ex.Message);
+                return false;
+            }
+
+
+        }
+    }
+}
---
+++ Formularios/frmMain.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Port_manager.Formularios
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCerrar_sesion = new System.Windows.Forms.Button();
+            this.lbbienvenida = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCerrar_sesion
+            // 
+            this.btnCerrar_sesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnCerrar_sesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar_sesion.Location = new System.Drawing.Point(1014, 688);
+            this.btnCerrar_sesion.Name = "btnCerrar_sesion";
+            this.btnCerrar_sesion.Size = new System.Drawing.Size(181, 37);
+            this.btnCerrar_sesion.TabIndex = 0;
+            this.btnCerrar_sesion.Text = "Cerrar sesión";
+            this.btnCerrar_sesion.UseVisualStyleBackColor = false;
+            this.btnCerrar_sesion.Click += new System.EventHandler(this.btnCerrar_sesion_Click);
+            // 
+            // lbbienvenida
+            // 
+            this.lbbienvenida.AutoSize = true;
+            this.lbbienvenida.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbbienvenida.Location = new System.Drawing.Point(3, 0);
+            this.lbbienvenida.Name = "lbbienvenida";
+            this.lbbienvenida.Size = new System.Drawing.Size(96, 38);
+            this.lbbienvenida.TabIndex = 1;
+            this.lbbienvenida.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Controls.Add(this.lbbienvenida);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1207, 44);
+            this.panel1.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(1207, 746);
+            this.Controls.Add(this.btnCerrar_sesion);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Port manager";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCerrar_sesion;
+        private System.Windows.Forms.Label lbbienvenida;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
