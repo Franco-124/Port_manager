723084f7746ea07668a91ea063d2fb74c90b5702
Johan <johan.franco@dsintegrations.com>
2025-04-25 21:55:28 -0500

    feat: Informe de usuarios cargado al data grid VERSION 25/04/2025

--- Formularios/frmGestionUsuario.Designer.cs
+++ Formularios/frmGestionUsuario.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblRegistro = new System.Windows.Forms.Label();
             this.dtgUsuarios = new System.Windows.Forms.DataGridView();
@@ -47,8 +45,9 @@
             this.panel1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 56);
+            this.panel1.Size = new System.Drawing.Size(1067, 69);
             this.panel1.TabIndex = 0;
             // 
             // lblRegistro
@@ -57,67 +56,66 @@
             this.lblRegistro.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.lblRegistro.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRegistro.ForeColor = System.Drawing.Color.White;
-            this.lblRegistro.Location = new System.Drawing.Point(292, 14);
+            this.lblRegistro.Location = new System.Drawing.Point(389, 17);
+            this.lblRegistro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblRegistro.Name = "lblRegistro";
-            this.lblRegistro.Size = new System.Drawing.Size(229, 29);
+            this.lblRegistro.Size = new System.Drawing.Size(282, 36);
             this.lblRegistro.TabIndex = 3;
             this.lblRegistro.Text = "Informe de Usuarios";
             // 
             // dtgUsuarios
             // 
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dtgUsuarios.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle3;
-            this.dtgUsuarios.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(60)))));
             this.dtgUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dtgUsuarios.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.id_usuario,
             this.nombre_usuario,
             this.email,
             this.rol});
-            this.dtgUsuarios.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.dtgUsuarios.Location = new System.Drawing.Point(45, 116);
+            this.dtgUsuarios.GridColor = System.Drawing.SystemColors.ButtonFace;
+            this.dtgUsuarios.Location = new System.Drawing.Point(31, 86);
             this.dtgUsuarios.Name = "dtgUsuarios";
-            this.dtgUsuarios.RowHeadersVisible = false;
             this.dtgUsuarios.RowHeadersWidth = 51;
-            this.dtgUsuarios.Size = new System.Drawing.Size(713, 268);
+            this.dtgUsuarios.RowTemplate.Height = 24;
+            this.dtgUsuarios.Size = new System.Drawing.Size(959, 285);
             this.dtgUsuarios.TabIndex = 1;
             // 
             // id_usuario
             // 
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopCenter;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.id_usuario.DefaultCellStyle = dataGridViewCellStyle4;
             this.id_usuario.HeaderText = "ID";
+            this.id_usuario.MinimumWidth = 6;
             this.id_usuario.Name = "id_usuario";
-            this.id_usuario.Width = 60;
+            this.id_usuario.Width = 150;
             // 
             // nombre_usuario
             // 
             this.nombre_usuario.HeaderText = "Usuario";
+            this.nombre_usuario.MinimumWidth = 6;
             this.nombre_usuario.Name = "nombre_usuario";
-            this.nombre_usuario.Width = 250;
+            this.nombre_usuario.Width = 150;
             // 
             // email
             // 
             this.email.HeaderText = "Email";
+            this.email.MinimumWidth = 6;
             this.email.Name = "email";
-            this.email.Width = 250;
+            this.email.Width = 180;
             // 
             // rol
             // 
             this.rol.HeaderText = "Rol";
+            this.rol.MinimumWidth = 6;
             this.rol.Name = "rol";
             this.rol.Width = 150;
             // 
             // frmGestionUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(33)))), ((int)(((byte)(74)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.dtgUsuarios);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmGestionUsuario";
             this.Text = "Usuarios Registrados";
             this.panel1.ResumeLayout(false);
--- Formularios/frmGestionUsuario.cs
+++ Formularios/frmGestionUsuario.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SqlConnectiondb;
 
 namespace Port_manager.Formularios
 {
@@ -15,6 +18,30 @@ namespace Port_manager.Formularios
         public frmGestionUsuario()
         {
             InitializeComponent();
+            cargar_datos();
+        }
+
+        
+
+        public void cargar_datos()
+        {
+            string consulta = "select id_usuario, nombre_usuario, email, rol from usuarios";
+            using (SqlConnection conexion = DatabaseHelper.GetConnection()) // Aquí usas tu método
+            {
+                SqlDataAdapter adaptador = new SqlDataAdapter(consulta, conexion);
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+
+                dtgUsuarios.AutoGenerateColumns = false; // Muy importante antes de asignar el DataSource
+                dtgUsuarios.DataSource = tabla;
+
+                
+                dtgUsuarios.Columns["id_usuario"].DataPropertyName = "id_usuario";
+                dtgUsuarios.Columns["nombre_usuario"].DataPropertyName = "nombre_usuario";
+                dtgUsuarios.Columns["email"].DataPropertyName = "email";
+                dtgUsuarios.Columns["rol"].DataPropertyName = "rol";
+
+            }
         }
     }
 }
